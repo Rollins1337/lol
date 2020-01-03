bb2a381446054d58b2d1a3d845c36059aa0f2a4c
Rollins1337 <57958635+Rollins1337@users.noreply.github.com>
2020-01-03 17:28:06 +0530

    Uploaded New Plugin

---
+++ belo.py
@@ -0,0 +1,422 @@
+"""
+Say something interesting...
+Syntax: .belo
+    by @Deonnn
+Quotes credits: Being logical Channel
+"""
+
+from telethon import events
+
+import asyncio
+
+import os
+
+import sys
+
+import random
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.belo", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    await event.edit("**__Typing...**__")
+
+    await asyncio.sleep(2)
+
+    x=(random.randrange(1,96))
+
+    if x==1:
+
+        await event.edit("**__\"Underwater bubbles and raindrops are total opposites of each other.\"**__")
+
+    if x==2:
+
+        await event.edit("**__\"If you buy an eraser you are literally paying for your mistakes.\"**__")
+
+    if x==3:
+
+        await event.edit("**__\"The Person you care for most has the potential to destroy you the most.\"**__")
+
+    if x==4:
+
+        await event.edit("**__\"If humans colonize the moon, it will probably attract retirement homes as the weaker gravity will allow the elderly to feel stronger.\"**__")
+
+    if x==5:
+
+        await event.edit("**__\"Any video with “wait for it” in the title is simply too long.\"**__")
+
+    if x==6:
+
+        await event.edit("**__\"Your age in years is how many times you’ve circled the Sun, but your age in months is how many times the Moon has circled you.\"**__")
+
+    if x==7:
+
+        await event.edit("**__\"Biting your tongue while eating is a perfect example of how you can still screw up, even with decades of experience.\"**__")
+
+    if x==8:
+
+        await event.edit("**__\"Saying that your home is powered by a wireless Nuclear fusion reactor that is 93 Million miles away sounds way cooler than just saying you have solar panels on your roof.\"**__")
+
+    if x==9:
+
+        await event.edit("**__\"The most crushing feeling is when someone smiles at you on the street and you don’t react fast enough to smile back.\"**__")
+
+    if x==10:
+
+        await event.edit("**__\"Teeth constantly require maintenance to prevent their decay when alive, and yet they manage to survive for thousands of years buried as fossils.\"**__")
+
+    if x==11:
+
+        await event.edit("**__\"A folder is for things that you don't want to fold.\"**__")
+
+    if x==12:
+
+        await event.edit("**__\"Waking up in the morning sometimes feels like resuming a shitty movie you decided to quit watching.\"**__")
+
+    if x==13:
+
+        await event.edit("**__\"If everything goes smoothly, you probably won't remember today.\"**__")
+
+    if x==14:
+
+        await event.edit("**__\"When you meet new people in real life, you unlock more characters for your dream world.\"**__")
+
+    if x==15:
+
+        await event.edit("**__\"Maybe if they renamed sunscreen to “anti-cancer cream” more people would wear it.\"**__")
+
+    if x==16:
+
+        await event.edit("**__\"200 years ago, people would never have guessed that humans in the future would communicate by silently tapping on glass.\"**__")
+
+    if x==17:
+
+        await event.edit("**__\"Parents worry about what their sons download and worry about what their daughters upload.\"**__")
+
+    if x==18:
+
+        await event.edit("**__\"It's crazy how you can be the same age as someone, but at a completely different stage in your life.\"**__")
+
+    if x==19:
+
+        await event.edit("**__\"When you think you wanna die, you really don't wanna die, you just don't wanna live like this.\"**__")
+
+    if x==20:
+
+        await event.edit("**__\"Technically, no one has ever been in an empty room.\"**__")
+
+    if x==21:
+
+        await event.edit("**__\"An onion is the bass player of food. You would probably not enjoy it solo, but you’d miss it if it wasn’t there.\"**__")
+
+    if x==22:
+
+        await event.edit("**__\"We run everywhere in videogames because we're too lazy to walk, but In real life we walk everywhere because we're too lazy to run.\"**__")
+
+    if x==23:
+
+        await event.edit("**__\"Every single decision you ever made has brought you to read this sentence.\"**__")
+
+    if x==24:
+
+        await event.edit("**__\"The word 'quiet' is often said very loud.\"**__")
+
+    if x==25:
+
+        await event.edit("**__\"Everybody wants you to work hard, but nobody wants to hear about how hard you work.\"**__")
+
+    if x==26:
+
+        await event.edit("**__\"We brush our teeth with hair on a stick and brush our hair with teeth on a stick.\"**__")
+
+    if x==27:
+
+        await event.edit("**__\"No one remembers your awkward moments but they’re too busy remembering their own.\"**__")
+
+    if x==28:
+
+        await event.edit("**__\"Dumb people try to say simple ideas as complex as possible while smart people try to say complex ideas as simple as possible.\"**__")
+
+    if x==29:
+
+        await event.edit("**__\"Some people think they're better than you because they grew up richer. Some people think they're better than you because they grew up poorer.\"**__")
+
+    if x==30:
+
+        await event.edit("**__\"The biggest irony is that computers & mobiles were invented to save out time!\"**__")
+
+    if x==31:
+
+        await event.edit("**__\"After honey was first discovered, there was likely a period where people were taste testing any available slime from insects.\"**__")
+
+    if x==32:
+
+        await event.edit("**__\"You know you’re getting old when your parents start disappointing you, instead of you disappointing them.\"**__")
+
+    if x==33:
+
+        await event.edit("**__\"Humans are designed to learn through experience yet the education system has made it so we get no experience.\"**__")
+
+    if x==34:
+
+        await event.edit("**__\"By focusing on blinking, you blink slower... Same for breathing.\"**__")
+
+    if x==35:
+
+        await event.edit("**__\"Drivers in a hurry to beat traffic usually cause the accidents which create the traffic they were trying to avoid.\"**__")
+
+    if x==36:
+
+        await event.edit("**__\"Characters that get married in fiction were literally made for each other.\"**__")
+
+    if x==37:
+
+        await event.edit("**__\"Babies are a clean hard drive that can be programmed with any language.\"**__")
+
+    if x==38:
+
+        await event.edit("**__\"There could be a miracle drug that cures every disease to man, that we'll never know about because it doesn't work on rats.\"**__")
+
+    if x==39:
+
+        await event.edit("**__\"Rhinos evolved to grow a horn for protection, but it's what's making them go extinct.\"**__")
+
+    if x==40:
+
+        await event.edit("**__\"Maybe we don't find time travelers because we all die in 25-50 years.\"**__")
+
+    if x==41:
+
+        await event.edit("**__\"Sleep is the trial version of death, It even comes with ads based on your activity.\"**__")
+
+    if x==42:
+
+        await event.edit("**__\"The most unrealistic thing about Spy movies is how clean the air ventilation system is!\"**__")
+
+    if x==43:
+
+        await event.edit("**__\"In games we play through easy modes to unlock hard modes. In life we play through hard modes to unlock easy modes.\"**__")
+
+    if x==44:
+
+        await event.edit("**__\"Silent people seem smarter than loud people, because they keep their stupid thoughts to themselves.\"**__")
+
+    if x==45:
+
+        await event.edit("**__\"If Greenland actually turns green, we're all screwed.\"**__")
+
+    if x==46:
+
+        await event.edit("**__\"If someone says clever things in your dream, it actually shows your own cleverness.\"**__")
+
+    if x==47:
+
+        await event.edit("**__\"Famous movie quotes are credited to the actor and not the actual writer who wrote them.\"**__")
+
+    if x==48:
+
+        await event.edit("**__\"No one actually teaches you how to ride a bicycle. They just hype you up until you work it out.\"**__")
+
+    if x==49:
+
+        await event.edit("**__\"Ask yourself why the the brain ignores the second the.\"**__")
+
+    if x==50:
+
+        await event.edit("**__\"You’ve probably forgot about 80% of your entire life and most of the memories you do remember are not very accurate to what actually happened.\"**__")
+
+    if x==51:
+
+        await event.edit("**__\"It will be a lot harder for kids to win against their parents in video games in the future.\"**__")
+
+    if x==52:
+
+        await event.edit("**__\"Everyone has flaws, if you don't recognize yours, you have a new one.\"**__")
+
+    if x==53:
+
+        await event.edit("**__\"Raising a child is training your replacement.\"**__")
+
+    if x==54:
+
+        await event.edit("**__\"'O'pen starts with a Closed circle, and 'C'lose starts with an open circle.\"**__")
+
+    if x==55:
+
+        await event.edit("**__\"There's always someone who hated you for no reason, and still does.\"**__")
+
+    if x==56:
+
+        await event.edit("**__\"After popcorn was discovered, there must have been a lot of random seeds that were roasted to see if it would have the same effect.\"**__")
+
+    if x==57:
+
+        await event.edit("**__\"The more important a good night's sleep is, the harder it is to fall asleep.\"**__")
+
+    if x==58:
+
+        await event.edit("**__\"Blessed are those that can properly describe the type of haircut they want to a new stylist.\"**__")
+
+    if x==59:
+
+        await event.edit("**__\"Too many people spend money they haven't earned, to buy things they don't want, to impress people they don't like!\"**__")
+
+    if x==60:
+
+        await event.edit("**__\"Theme park employees must be good at telling the difference between screams of horror and excitement.\"**__")
+
+    if x==61:
+
+        await event.edit("**__\"6 to 6:30 feels more half-an-hour than 5:50 to 6:20\"**__")
+
+    if x==62:
+
+        await event.edit("**__\"Getting your password right on the last login attempt before lockout is the closest thing to disarming a bomb at the last minute that most of us will experience.\"**__")
+
+    if x==63:
+
+        await event.edit("**__\"Listening to podcasts before bed is the adult version of story-time.\"**__")
+
+    if x==64:
+
+        await event.edit("**__\"If all criminals stopped robbing then the security industry would fall in which they could then easily go back to robbing.\"**__")
+
+    if x==65:
+
+        await event.edit("**__\"A ton of whales is really only like half a whale.\"**__")
+
+    if x==66:
+
+        await event.edit("**__\"When you get old, the old you is technically the new you, and your young self is the old you.\"**__")
+
+    if x==67:
+
+        await event.edit("**__\"You probably won't find many negative reviews of parachutes on the Internet.\"**__")
+
+    if x==68:
+
+        await event.edit("**__\"We show the most love and admiration for people when they're no longer around to appreciate it.\"**__")
+
+    if x==69:
+
+        await event.edit("**__\"We've practiced sleeping thousands of times, yet can't do it very well or be consistent.\"**__")
+
+    if x==70:
+
+        await event.edit("**__\"Humans are more enthusiastic about moving to another planet with hostile environment than preserving earth - the planet they are perfectly shaped for.\"**__")
+
+    if x==71:
+
+        await event.edit("**__\"The happiest stage of most people's lives is when their brains aren't fully developed yet.\"**__")
+
+    if x==72:
+
+        await event.edit("**__\"The most effective alarm clock is a full bladder.\"**__")
+
+    if x==73:
+
+        await event.edit("**__\"You probably just synchronized blinks with millions of people.\"**__")
+
+    if x==74:
+
+        await event.edit("**__\"Since we test drugs on animals first, rat medicine must be years ahead of human medicine.\"**__")
+
+    if x==75:
+
+        await event.edit("**__\"Night before a day off is more satisfying than the actual day off.\"**__")
+
+    if x==76:
+
+        await event.edit("**__\"We put paper in a folder to keep it from folding.\"**__")
+
+    if x==77:
+
+        await event.edit("**__\"Somewhere, two best friends are meeting for the first time.\"**__")
+
+    if x==78:
+
+        await event.edit("**__\"Our brain simultaneously hates us, loves us, doesn't care about us, and micromanages our every move.\"**__")
+
+    if x==79:
+
+        await event.edit("**__\"Being a male is a matter of birth. Being a man is a matter of age. But being a gentleman is a matter of choice.\"**__")
+
+    if x==80:
+
+        await event.edit("**__\"Soon the parents will be hiding their social account from their kids rather than kids hiding their accounts from the parents.\"**__")
+
+    if x==81:
+
+        await event.edit("**__\"Wikipedia is what the internet was meant to be.\"**__")
+
+    if x==82:
+
+        await event.edit("**__\"A theme park is the only place that you can hear screams in the distance and not be concerned.\"**__")
+
+    if x==83:
+
+        await event.edit("**__\"A wireless phone charger offers less freedom of movement than a wired one.\"**__")
+
+    if x==84:
+
+        await event.edit("**__\"If you repeatedly criticize someone for liking something you don't, they won't stop liking it. They'll stop liking you.\"**__")
+
+    if x==85:
+
+        await event.edit("**__\"Somewhere there is a grandmother, whose grandson really is the most handsome boy in the world.\"**__")
+
+    if x==86:
+
+        await event.edit("**__\"If someday human teleportation becomes real, people will still be late for work.\"**__")
+
+    if x==87:
+
+        await event.edit("**__\"The first humans who ate crabs must have been really hungry to try and eat an armored sea spider\"**__")
+
+    if x==88:
+
+        await event.edit("**__\"Doing something alone is kind of sad, but doing it solo is cool af.\"**__")
+
+    if x==89:
+
+        await event.edit("**__\"Your brain suddenly becomes perfect at proofreading after you post something.\"**__")
+
+    if x==90:
+
+        await event.edit("**__\"There's always that one song in your playlist that you always skip but never remove.\"**__")
+
+    if x==91:
+
+        await event.edit("**__\"Kids next century will probably hate us for taking all the good usernames.\"**__")
+
+    if x==92:
+
+        await event.edit("**__\"Bubbles are to fish what rain is to humans.\"**__")
+
+    if x==93:
+
+        await event.edit("**__\"The more people you meet, the more you realise and appreciate how well your parents raised you.\"**__")
+
+    if x==94:
+
+        await event.edit("**__\"A comma is a short pause, a coma is a long pause.\"**__")
+
+    if x==95:
+
+        await event.edit("**__\"Someday you will either not wake up or not go to sleep.\"**__")     
+
+    if x==96:
+
+        await event.edit("**__\"Bermuda Triangle might be the exit portal of this simulation.\"**__")
+    
+    if x==97:
+
+        await event.edit("**__\"If we put solar panels above parking lots, then our cars wouldn't get hot and we would have a lot of clean energy.\"**__")
+
+    
